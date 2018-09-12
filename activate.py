7eb78ca9a612966343d79020ce4a4c9fe0bf5bd2
Bart Spaans <bart.spaans@gmail.com>
2018-09-12 17:12:27 +0100

    Implement the Azure provider

---
+++ activate.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python 
+
+import sys
+import json
+import os
+import base64
+import subprocess
+import tempfile
+
+output_location = sys.argv[1]
+credential_string = os.environ["INPUT_credentials"]
+subscription_id = os.environ["INPUT_subscription_id"]
+credentials = json.loads(credential_string)
+app_id = credentials["appId"]
+password = credentials["password"]
+tenant_id = credentials["tenant"]
+
+def run_az(cmd, **kwargs):
+    gcloud_cmd = ["az"]
+    env = os.environ
+    for k, v in kwargs.iteritems():
+        env[k] = v
+    print "Executing", " ".join(gcloud_cmd + cmd)
+    return subprocess.Popen(gcloud_cmd + cmd, env=env)
+
+print "Activating service account"
+
+_, tmp = tempfile.mkstemp()
+with open(tmp, 'w') as f:
+    f.write(credential_string)
+
+proc = run_az(["account", "set", "--subscription", subscription_id)
+if proc.wait() != 0:
+    sys.exit(1)
+proc = run_az(["login", "--service-principal", "-u", app_id, "-p", password, "--tenant", tenant_id])
+if proc.wait() != 0:
+    sys.exit(1)
+
+os.unlink(tmp)
+
+result = {
+    "client_id": credentials["appId"],
+    "client_secret": credentials["password"],
+    "tenant_id": credentials["tenant"],
+    "subscription_id": subscription_id,
+    "service_principal_name": credentials["name"],
+}
+
+for key, value in result.iteritems():
+    print "Setting output", key
+
+print output_location
+with open(output_location, 'w') as f:
+    f.write(json.dumps(result))
